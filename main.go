c0ce2e53fad3ba33208fd3f1ca4c7376a82e2913
Junwei Zhao <wahyd4@gmail.com>
2022-03-14 20:26:43 +1100

    Use env to inject parameters just to make it simple

--- main.go
+++ main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/wahyd4/mryum/calculator"
 )
 
-const (
-	filePath  = "./testx3.txt"
-	topNumber = 4
+var (
+	filePath  = getEnvWithDefault("INPUT", "./test3.txt")
+	topNumber = getEnvWithDefault("TOPX", "3")
 )
 
 func main() {
+	topNumber, err := strconv.Atoi(topNumber)
+	if err != nil {
+		fmt.Println("TOPX must be a number")
+	}
+
 	topNumbers, err := calculator.CalculateLargestNumbers(filePath, topNumber)
 	if err != nil {
 		fmt.Println("Oops: " + err.Error())
 		os.Exit(1)
 	}
 	fmt.Printf("Top %d numbers are: %v", topNumber, topNumbers)
+}
+
+func getEnvWithDefault(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
